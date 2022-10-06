bb94c3f4be0694a1eca777f6bc84b68dcdccdd1c
f0rmul <92054017+f0rmul@users.noreply.github.com>
2022-10-06 20:05:18 +0300

    Update UserHttpController.hpp

--- src/InterfaceLayer/Controllers/UserHttpController.hpp
+++ src/InterfaceLayer/Controllers/UserHttpController.hpp
@@ -12,11 +12,11 @@ public:
 	UserHttpController() : userServicePtr{ std::make_unique<UserManagmentService>() } {}
 
 	METHOD_LIST_BEGIN
-		ADD_METHOD_TO(UserHttpController::create_user,  "/new",	           { drogon::Post   });
-	    ADD_METHOD_TO(UserHttpController::get_user,     "/get/user",	   { drogon::Get    });
-		ADD_METHOD_TO(UserHttpController::get_all_users,"/get/users",      { drogon::Get    });
-		ADD_METHOD_TO(UserHttpController::update_user,  "/update/user",    { drogon::Patch  });
-		ADD_METHOD_TO(UserHttpController::delete_user,  "/delete/user",    { drogon::Delete });
+		ADD_METHOD_TO(UserHttpController::create_user,  "/user" ,   { drogon::Post   });
+	    ADD_METHOD_TO(UserHttpController::get_user,         "/user" ,   { drogon::Get    });
+		ADD_METHOD_TO(UserHttpController::get_all_users,"/users",   { drogon::Get    });
+		ADD_METHOD_TO(UserHttpController::update_user,  "/user",    { drogon::Patch  });
+		ADD_METHOD_TO(UserHttpController::delete_user,  "/user",    { drogon::Delete });
 	METHOD_LIST_END
 	
 	drogon::Task<ResponseType> create_user  (drogon::HttpRequestPtr);
